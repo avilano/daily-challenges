fca5a4ea02980eed81b1e5577c21e1b4285cc4d1
avilano <vira0092@gmail.com>
2016-07-07 10:43:59 -0500

    updating repo

--- leap.js
+++ leap.js
@@ -1,21 +1,54 @@
+function isLeap(year){
 
-/*
-ALBERTO E. VILANO REYNA
-Leap Year Calculator!
-*/
+    if ( (year%4) == 0 && (year%100) != 0 || (year%400) == 0){
 
-var prompt = require('prompt');
-
-prompt.start();
-
-prompt.get(['year'], function (err, usrinput) {
-
-    if ( (usrinput.year%4) == 0 && (usrinput.year%100) != 0 || (usrinput.year%400) == 0){
-
-      console.log("The year: " + usrinput.year + " is a leap year!!");
+      return true;
     }
     else {
-      console.log("The year: " + usrinput.year + " is NOT a leap year!!");
-    }
+		 return false;
+	 }
 
- });
+ };
+
+/// -- do not edit below ---
+
+describe('Leap year', function() {
+
+  it('is not very common', function() {
+    expect(isLeap(2015)).toBe(false);
+  });
+
+  it('is introduced every 4 years to adjust about a day', function() {
+    expect(isLeap(2016)).toBe(true);
+  });
+
+  it('is skipped every 100 years to remove an extra day', function() {
+    expect(isLeap(1900)).toBe(false);
+  });
+
+  it('is reintroduced every 400 years to adjust another day', function() {
+    expect(isLeap(2000)).toBe(true);
+  });
+
+  // Feel free to enable the following tests to check some more examples
+  describe('Additional example of a leap year that', function () {
+
+    it('is not a leap year', function () {
+      expect(isLeap(1978)).toBe(false);
+    });
+
+    it('is a common leap year', function () {
+      expect(isLeap(1992)).toBe(true);
+    });
+
+    it('is skipped every 100 years', function () {
+      expect(isLeap(2100)).toBe(false);
+    });
+
+    it('is reintroduced every 400 years', function () {
+      expect(isLeap(2400)).toBe(true);
+    });
+
+  });
+
+});
